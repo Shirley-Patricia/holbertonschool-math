b95011253c50fc6894d891b23772111640d2b7c1
Shirley <shircar16@gmail.com>
2021-07-30 18:07:31 -0500

    change position

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,10 +10,16 @@
 
 void display_complex_number(complex c)
 {
-	if (c.im == 0)
-		printf("%.0f\n", c.re);
-	if (c.re == 0)
-		printf("%.0fi\n", c.im);
 	if (c.im != 0 && c.re != 0)
+	{
 		printf("%.0f + %.0fi\n", c.re, c.im);
+	}	
+	else if (c.im == 0)
+	{
+		printf("%.0f\n", c.re); 
+	}
+	else if (c.re == 0)
+	{
+		printf("%.0fi\n", c.im);
+	}	
 }
