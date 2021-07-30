0de7813df121bbb5f6da3b8284667a0484196c83
Shirley <shircar16@gmail.com>
2021-07-30 17:41:43 -0500

    display complex numbers

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* display_complex_number- initialize a variable.
+* @c: name struct complex
+* 
+* Return: no return.
+*/
+
+void display_complex_number(complex c)
+{
+	if (c.im == 0)
+		printf("%.0f\n", c.re);
+	if (c.re == 0)
+		printf("%.0fi\n", c.im);
+	if (c.im != 0 && c.re != 0)
+		printf("%.0f + %.0fi\n", c.re, c.im);
+}
