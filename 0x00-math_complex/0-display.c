01ddc830a4b52c0617deba331475059a911b0df4
Shirley <shircar16@gmail.com>
2021-07-30 21:14:06 -0500

    option im iqual 1 or -1

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,22 +10,32 @@
 
 void display_complex_number(complex c)
 {
-	if (c.re == 0 && c.im != 0)
+	if (c.im == 0 && c.re == 0)
+	{}
+	if(c.re == 0 && c.im != 0)
 	{
-		printf("%.0fi\n", c.im);
+		if (c.im == 1)
+			printf("i\n");
+		else if (c.im == -1)
+			printf("-i\n");
+		else if (c.im < 0)
+			printf("-%.0fi\n", c.im * -1);
+		else 
+			printf("%.0fi\n", c.im);
 	}
 	else if (c.re != 0 && c.im == 0)
 	{
 		printf("%.0f\n", c.re);
 	}
-	else if  (c.im < 0)
+	else if (c.re != 0 && c.im != 0)
 	{
-		printf("%.0f - %.0fi\n", c.re, c.im * -1);
+		if (c.im == 1)
+			printf("%.0f + i\n", c.re);
+		else if (c.im == -1)
+			printf("%.0f - i\n", c.re);
+		else if (c.im < 0)
+			printf("%.0f - %.0fi\n", c.re, c.im * -1);
+		else
+			printf("%.0f + %.0fi\n", c.re, c.im);
 	}
-	else if (c.im == 0 && c.re == 0)
-	{
-		return;
-	}
-	else
-		printf("%.0f + %.0fi\n", c.re, c.im);
 }
