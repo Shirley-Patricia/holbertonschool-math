4d596005a704fea0faa9dd833b7af3baf9b900cf
Shirley <shircar16@gmail.com>
2021-07-30 23:01:50 -0500

    argument of a complex number

---
+++ 0x00-math_complex/3-argument.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+* argument - argument of a complex number .
+* @c: name struct complex
+*
+* Return: argument.
+*/
+
+double argument(complex c)
+{
+	double a;
+	if (c.re != 0)
+	{
+		a = atan2(c.im, c.re);
+	}
+	return (a);
+}
